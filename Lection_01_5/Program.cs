11f12932051d70bf0069f791a65c1dd3cf4b4c6d
Rady <Rady2004@gmail.com>
2023-12-15 17:45:58 +0500

    add Lection_01_5 if and else

---
+++ Lection_01_5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "рыжий")
+{
+    Console.WriteLine("Ура, это Рыжий");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
